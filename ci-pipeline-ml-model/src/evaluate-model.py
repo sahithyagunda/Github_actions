412a0933d7591e212156c21b8594124afeedcf3e
sahithya <gundasahithya908@gmail.com>
2025-11-12 16:38:16 +0530

    fixed for lints

--- ci-pipeline-ml-model/src/evaluate-model.py
+++ ci-pipeline-ml-model/src/evaluate-model.py
@@ -1,16 +1,14 @@
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pickle
 
-
 def evaluate_saved_model():
     data = load_wine()
-    _,X_test,_,y_test = train_test_split(data.data,data.target,test_size=0.2,random_state=42)
+    _,X_test,_,y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
     model = pickle.load(open("model.pkl",'rb'))
     predictions = model.predict(X_test)
-    accuracy = accuracy_score(y_test,predictions)
+    accuracy = accuracy_score(y_test, predictions)
     return accuracy
 
 
--- ci-pipeline-ml-model/src/train_model.py
+++ ci-pipeline-ml-model/src/train_model.py
@@ -7,25 +7,25 @@ import pickle
 
 def loading_data():
     data = load_wine()
-    X_train,X_test,y_train,y_test = train_test_split(data.data,data.target,test_size=0.2,random_state=42)
-    return X_train,X_test,y_train,y_test
+    X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
 
 
-def train(X_train,y_train):
+def train(X_train, y_train):
     model = RandomForestClassifier(n_estimators=50)
-    model.fit(X_train,y_train)
+    model.fit(X_train, y_train)
     return model
 
 
-def evaluate(model,X_test,y_test):
+def evaluate(model, X_test, y_test):
     predictions = model.predict(X_test)
-    accuracy = accuracy_score(y_test,predictions)
+    accuracy = accuracy_score(y_test, predictions)
     return accuracy
 
 
 if __name__ == "__main__":
     X_train,X_test,y_train,y_test = loading_data()
-    model = train(X_train,y_train)
-    accuracy = evaluate(model,X_test,y_test)
-    pickle.dump(model,open('model.pkl','wb'))
+    model = train(X_train, y_train)
+    accuracy = evaluate(model, X_test, y_test)
+    pickle.dump(model, open('model.pkl', 'wb'))
     print(f"Model trained successfully with accuracy {accuracy:2f}")
